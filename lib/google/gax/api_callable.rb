fe418864fc6e8e8780268b5e04258ebe190a9a87
Jun Mukai <mukai@google.com>
2016-04-04 16:30:07 -0700

    Add the page_streaming test case
    Change-Id: I6e8760d43ed9282b6647e0daf4fce03703532dd8

--- lib/google/gax/api_callable.rb
+++ lib/google/gax/api_callable.rb
@@ -1,3 +1,32 @@
+# Copyright 2016, Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 require 'google/gax'
 require 'google/gax/errors'
 
@@ -53,20 +82,17 @@ module Google
       a_func,
       request_page_token_field,
       response_page_token_field,
-      resource_field,
-      timeout)
-      with_timeout = _add_timeout_arg(a_func, timeout)
-      proc do |*args|
-        request = args[0]
+      resource_field)
+      proc do |request|
         Enumerator.new do |y|
           loop do
-            response = with_timeout.call(request)
+            response = a_func.call(request)
             response.send(resource_field).each do |obj|
               y << obj
             end
             next_page_token = response.send(response_page_token_field)
             break unless next_page_token
-            request.send(request_page_token_field + '=', next_page_token)
+            request.send(request_page_token_field.to_s + '=', next_page_token)
           end
         end
       end
