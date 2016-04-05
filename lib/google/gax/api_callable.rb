a37d9d28032b228fad8ebdcf0b10072f345e6330
Jun Mukai <mukai@google.com>
2016-04-04 17:59:57 -0700

    Import doc-comment from Python api_callable
    Change-Id: I2ccf94ec766b8c8f3dac6a011e5e7c0f473fd2ca

--- lib/google/gax/api_callable.rb
+++ lib/google/gax/api_callable.rb
@@ -33,6 +33,29 @@ require 'google/gax/errors'
 module Google
   module Gax
     # rubocop:disable Metrics/AbcSize
+
+    # Converts an rpc call into an API call governed by the settings.
+    #
+    # In typical usage, +func+ will be a proc used to make an rpc request.
+    # This will mostly likely be a bound method from a request stub used to make
+    # an rpc call.
+    #
+    # The result is created by applying a series of function decorators
+    # defined in this module to +func+.  +settings+ is used to determine
+    # which function decorators to apply.
+    #
+    # The result is another proc which for most values of +settings+ has the
+    # same signature as the original. Only when +settings+ configures bundling
+    # does the signature change.
+    #
+    # Args::
+    #   +func+:: is used to make a bare rpc call
+    #   +settings+:: provides the settings for this call
+    # Returns::
+    #   a bound method on a request stub used to make an rpc call
+    # Raises::
+    #   StandardError:: if +settings+ has incompatible values, e.g, if bundling
+    #                   and page_streaming are both configured
     def create_api_call(func, settings)
       api_call = if settings.retry_codes?
                    _retryable(func, settings.retry_options)
@@ -60,6 +83,13 @@ module Google
       _catch_errors(api_call)
     end
 
+    # Updates a_func to wrap exceptions with GaxError
+    #
+    # Args::
+    #   a_func:: A proc.
+    #   errors:: Configures the exceptions to wrap.
+    # Returns::
+    #   A proc that will wrap certain exceptions with GaxError
     def _catch_errors(a_func, errors:StandardError)
       proc do |*args|
         begin
@@ -70,6 +100,22 @@ module Google
       end
     end
 
+    # Creates a proc that transforms an API call into a bundling call.
+    #
+    # It transform a_func from an API call that receives the requests and
+    # returns the response into a proc that receives the same request, and
+    # returns a +Google::Gax::Bundling::Event+.
+    #
+    # The returned Event object can be used to obtain the eventual result of the
+    # bundled call.
+    #
+    # Args::
+    #   +a_func+:: an API call that supports bundling.
+    #   +desc+:: describes the bundling that +a_func+ supports.
+    #   +bundler+:: orchestrates bundling.
+    #
+    # Returns::
+    #   A proc takes the API call's request and returns an Event object.
     def _bundleable(a_func, desc, bundler)
       proc do |request|
         the_id = bundling.compute_bundle_id(
@@ -79,6 +125,17 @@ module Google
       end
     end
 
+    # Creates a proc that yields an iterable to performs page-streaming.
+    #
+    # Args::
+    #   +a_func+:: an API call that is page streaming.
+    #   +request_page_token_field+:: The field of the page token in the request.
+    #   +response_page_token_field+::
+    #     The field of the next page token in the response.
+    #   +resource_field+:: The field to be streamed.
+    #
+    # Returns::
+    #   A proc that returns an iterable over the specified field.
     def _page_streamable(
       a_func,
       request_page_token_field,
@@ -99,6 +156,17 @@ module Google
       end
     end
 
+    # Creates a proc equivalent to a_func, but that retries on certain
+    # exceptions.
+    #
+    # Args::
+    #   +a_func+:: A proc.
+    #   +retry_options+::
+    #     Configures the exceptions upon which the proc should retry,
+    #     and the parameters to the exponential backoff retry algorithm.
+    #
+    # Returns::
+    #   A proc that will retry on exception.
     def _retryable(a_func, retry_options)
       max_attempts = (retry_options.backoff_settings.total_timeout_millis /
         retry_options.backoff_settings.initial_rpc_timeout_millis).to_i
@@ -116,6 +184,17 @@ module Google
       end
     end
 
+    # Updates +a_func+ so that it gets called with the timeout as its final arg.
+    #
+    # This converts a proc, a_func, into another proc with an additional
+    # positional arg.
+    #
+    # Args::
+    #   +a_func+:: a proc to be updated
+    #   +timeout+:: to be added to the original proc as it final positional arg.
+    #
+    # Returns::
+    #   the original proc updated to the timeout arg
     def _add_timeout_arg(a_func, timeout)
       proc do |*args|
         updated_args = args + [timeout]
